019e440e6fe1a85dc553cc3cbaea17d8608285da
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-02-02 14:59:47 +0300

    feat: Add interaction system

---
+++ Assets/_Project/InteractionSystem/Scripts/InteractionManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using TwinStickShooter.InteractionSystem.Interfaces;
+using UnityEngine;
+
+namespace TwinStickShooter.InteractionSystem
+{
+    public class InteractionManager : MonoBehaviour
+    {
+        private readonly List<IInteractable> _possibleInteractables = new();
+
+        public void AddInteractable(IInteractable interactable)
+        {
+            if (_possibleInteractables.Contains(interactable)) return;
+            _possibleInteractables.Add(interactable);
+        }
+
+        public void RemoveInteractable(IInteractable interactable)
+        {
+            _possibleInteractables.Remove(interactable);
+        }
+
+        private void Interact()
+        {
+            if (_possibleInteractables.Count == 0) return;
+            SortThePossibleInteractables();
+            var interactable = _possibleInteractables[0];
+            interactable.Interact();
+        }
+
+        private void SortThePossibleInteractables()
+        {
+            var originPos = transform.position;
+            _possibleInteractables.Sort(CompareDistance);
+
+            int CompareDistance(IInteractable a, IInteractable b)
+            {
+                var distA = (a.Position - originPos).sqrMagnitude;
+                var distB = (b.Position - originPos).sqrMagnitude;
+                return distA.CompareTo(distB);
+            }
+        }
+    }
+}
---
+++ Assets/_Project/InteractionSystem/Scripts/Interfaces/IInteractable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace TwinStickShooter.InteractionSystem.Interfaces
+{
+    public interface IInteractable
+    {
+        public Vector3 Position { get; }
+        public void Interact();
+    }
+}
