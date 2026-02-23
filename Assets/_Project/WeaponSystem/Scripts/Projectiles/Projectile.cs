727ff48f36eabe4683800c80dd713acc6fdef151
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-02-23 16:52:40 +0300

    feat: Create projectile controller and prefab

---
+++ Assets/_Project/WeaponSystem/Scripts/Projectiles/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using TwinStickShooter.DamageableSystem;
+using UnityEngine;
+
+namespace TwinStickShooter.WeaponSystem.Projectiles
+{
+    public class Projectile : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private bool _isActive;
+        private float _damage;
+        private float _maxDistance;
+        private float _distanceTravelled;
+
+        public void Fire(float damage, float maxDistance)
+        {
+            _damage = damage;
+            _maxDistance = maxDistance;
+            _distanceTravelled = 0f;
+            _isActive = true;
+        }
+
+        private void Disable()
+        {
+            if (!_isActive) return;
+            _isActive = false;
+            ProjectilePool.Instance.ReleaseProjectile(this);
+        }
+
+        #region MonoBehaviour Methods
+
+        private void Update()
+        {
+            if (!_isActive) return;
+            var moveDistance = speed * Time.deltaTime;
+            transform.position += (transform.forward * moveDistance);
+            _distanceTravelled += moveDistance;
+            if (_distanceTravelled < _maxDistance) return;
+            Disable();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_isActive || !other.TryGetComponent(out IDamageable target)) return;
+            target.Damage(_damage);
+            Disable();
+        }
+
+        #endregion
+    }
+}
