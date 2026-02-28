2f34c9cbc566a9dc011a8ce38fb18a207150c387
Abdülkadir Dursun <akadirdursn@gmail.com>
2026-02-28 04:09:31 +0300

    feat: Enable and disable aim rig with aim state

--- Assets/_Project/AnimationSystem/Scripts/IKAimTargetPlacer.cs
+++ Assets/_Project/AnimationSystem/Scripts/IKAimTargetPlacer.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 namespace TwinStickShooter.AnimationSystem
 {
     public class IKAimTargetPlacer : MonoBehaviour
     {
+        [SerializeField] private Rig aimRig;
         [SerializeField] private Transform aimTarget;
         [SerializeField] private float distanceFromBody = 10f;
 
         public void SetActive(bool value)
         {
+            aimRig.weight = value ? 1f : 0f;
         }
 
         public void SetDirection(Vector3 lookDirection)
